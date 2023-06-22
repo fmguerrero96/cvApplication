29043b75400e71de8934b7334bb94bb77bb1e312
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-21 21:05:13 -0400

    Add onChange and name props and also remove submit button

--- src/components/editPersonal.js
+++ src/components/editPersonal.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { useState } from "react";
 
-export default function EditPersonal({submitPersonal}) {
+export default function EditPersonal({onEditInfo}) {
     return(
         <div>
             <h2 className="PersonalTitle PersonalInfo">
                 Personal Information
             </h2>
-            <form className="personalForm" onSubmit={submitPersonal}>
-            <input className="personalInput firstName" type="text" placeholder="First Name" required/>
-            <input className="personalInput lastName" type="text" placeholder="Last Name" required/>
-            <input className="personalInput profession" type="text" placeholder="Profession" required/>
-            <input className="personalInput phoneNumber" type="text" placeholder="Phone Number" required/>
-            <input className="personalInput email" type="email" placeholder="e-mail" required/>
-            <button type="submit" className="submitPersonal btn">Update Resume</button>
+            <form className="personalForm">
+            <input onChange={onEditInfo} name="firstName" className="personalInput firstName" type="text" placeholder="First Name" required/>
+            <input onChange={onEditInfo} name="lastName" className="personalInput lastName" type="text" placeholder="Last Name" required/>
+            <input onChange={onEditInfo} name="profession" className="personalInput profession" type="text" placeholder="Profession" required/>
+            <input onChange={onEditInfo} name="phoneNumber" className="personalInput phoneNumber" type="text" placeholder="Phone Number" required/>
+            <input onChange={onEditInfo} name="email" className="personalInput email" type="email" placeholder="e-mail" required/>
             </form>
         </div>
     )
