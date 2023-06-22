e1f8f66ad518b533fe0a375a1fe1407205a88822
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-21 21:08:05 -0400

    Set state for personal info, creat handleChange function and import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { useState } from "react";
+import EditPersonal from "./components/editPersonal";
+import ViewPersonal from "./components/viewPersonal";
 
 function App() {
+
+  const [person, setPerson] = useState({
+    firstName: 'Your Name',
+    lastName: '',
+    profession: '',
+    phoneNumber: '',
+    email: '',
+  })
+
+  function handleChange(e) {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div >
+      <EditPersonal onEditInfo={handleChange}/>
+      <ViewPersonal firstName={person.firstName} lastName={person.lastName} profession={person.profession} phoneNumber={person.phoneNumber} email={person.email}/>
     </div>
   );
 }
