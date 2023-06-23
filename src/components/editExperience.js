458faf181374905316ba8d4daab30f6c87d06322
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-23 12:20:04 -0400

    Add and edit props

--- src/components/editExperience.js
+++ src/components/editExperience.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function EditExperience({onExperienceSubmit}) {
+export default function EditExperience({onExperienceSubmit, onExpereinceChange}) {
     return(
         <div className="editExperinceComponent">
             <h2 className="PersonalTitile experienceInfo">
                 Work Experience
             </h2>
             <form className="experienceForm" onSubmit={onExperienceSubmit}>
-                <input className="experienceInput companyName" type='text' placeholder="Company name"/>
-                <input className="experienceInput position" type='text' placeholder="Position"/>
+                <input onChange={onExpereinceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
+                <input onChange={onExpereinceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
                 <label className="fromLabel"> From: 
-                    <input className="experienceInput startDate" type='date'/>
+                    <input onChange={onExpereinceChange} name="startDate" className="experienceInput startDate" type='date'/>
                 </label>
                 <label className="toLabel">
                     To: 
-                    <input className="experienceInput startDate" type='date'/>
+                    <input onChange={onExpereinceChange} name="endDate" className="experienceInput endDate" type='date'/>
                 </label>
                 <button className="addExperience" type="submit" >Add to CV</button>
             </form>
