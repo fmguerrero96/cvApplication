ce6942a72346046791d9eda491e513cf39895b5e
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-24 23:03:41 -0400

    Add a required attribute to input fields

--- src/components/editExperience.js
+++ src/components/editExperience.js
@@ -7,14 +7,14 @@ export default function EditExperience({companyName, position, startDate, endDat
                 Work Experience
             </h2>
             <form className="experienceForm" onSubmit={onExperienceSubmit}>
-                <input value={companyName} onChange={onExperienceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
-                <input value={position} onChange={onExperienceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
+                <input required value={companyName} onChange={onExperienceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
+                <input required value={position} onChange={onExperienceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
                 <label className="fromLabel"> From: 
-                    <input value={startDate} onChange={onExperienceChange} name="startDate" className="experienceInput startDate" type='date'/>
+                    <input required value={startDate} onChange={onExperienceChange} name="startDate" className="experienceInput startDate" type='date'/>
                 </label>
                 <label className="toLabel">
                     To: 
-                    <input value={endDate} onChange={onExperienceChange} name="endDate" className="experienceInput endDate" type='date'/>
+                    <input required value={endDate} onChange={onExperienceChange} name="endDate" className="experienceInput endDate" type='date'/>
                 </label>
                 <button className="addExperience" type="submit" >Add to CV</button>
             </form>
