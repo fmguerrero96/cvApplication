5088a6cb62dea01916bfa229fba9abd2421e6300
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-24 22:29:45 -0400

    Pass in the correct onSubmit and onChange event handlers

--- src/components/editExperience.js
+++ src/components/editExperience.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function EditExperience({onExperienceSubmit, onExpereinceChange}) {
+export default function EditExperience({onExperienceSubmit, onExperienceChange}) {
     return(
         <div className="editExperinceComponent">
             <h2 className="PersonalTitile experienceInfo">
                 Work Experience
             </h2>
             <form className="experienceForm" onSubmit={onExperienceSubmit}>
-                <input onChange={onExpereinceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
-                <input onChange={onExpereinceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
+                <input onChange={onExperienceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
+                <input onChange={onExperienceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
                 <label className="fromLabel"> From: 
-                    <input onChange={onExpereinceChange} name="startDate" className="experienceInput startDate" type='date'/>
+                    <input onChange={onExperienceChange} name="startDate" className="experienceInput startDate" type='date'/>
                 </label>
                 <label className="toLabel">
                     To: 
-                    <input onChange={onExpereinceChange} name="endDate" className="experienceInput endDate" type='date'/>
+                    <input onChange={onExperienceChange} name="endDate" className="experienceInput endDate" type='date'/>
                 </label>
                 <button className="addExperience" type="submit" >Add to CV</button>
             </form>
