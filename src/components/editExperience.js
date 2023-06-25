5153e708b234f62f9100c1d0fa3028c24aaa1285
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-24 23:01:11 -0400

    Add value property to input elements

--- src/components/editExperience.js
+++ src/components/editExperience.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function EditExperience({onExperienceSubmit, onExperienceChange}) {
+export default function EditExperience({companyName, position, startDate, endDate, onExperienceSubmit, onExperienceChange}) {
     return(
         <div className="editExperinceComponent">
             <h2 className="PersonalTitile experienceInfo">
                 Work Experience
             </h2>
             <form className="experienceForm" onSubmit={onExperienceSubmit}>
-                <input onChange={onExperienceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
-                <input onChange={onExperienceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
+                <input value={companyName} onChange={onExperienceChange} name="companyName" className="experienceInput companyName" type='text' placeholder="Company name"/>
+                <input value={position} onChange={onExperienceChange} name="position" className="experienceInput position" type='text' placeholder="Position"/>
                 <label className="fromLabel"> From: 
-                    <input onChange={onExperienceChange} name="startDate" className="experienceInput startDate" type='date'/>
+                    <input value={startDate} onChange={onExperienceChange} name="startDate" className="experienceInput startDate" type='date'/>
                 </label>
                 <label className="toLabel">
                     To: 
-                    <input onChange={onExperienceChange} name="endDate" className="experienceInput endDate" type='date'/>
+                    <input value={endDate} onChange={onExperienceChange} name="endDate" className="experienceInput endDate" type='date'/>
                 </label>
                 <button className="addExperience" type="submit" >Add to CV</button>
             </form>
