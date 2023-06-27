f440fa614d59725089de8ca95044773f2e52302e
Misael Guerrero <mguerrerowest@gmail.com>
2023-06-27 15:38:32 -0400

    Pass in delete function prop

--- src/components/viewExperience.js
+++ src/components/viewExperience.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ViewExperience({experiences}) {
+export default function ViewExperience({experiences, onDeleteExp}) {
     return(
         <ul className="viewExperience">
             {experiences.map((exp) => (
@@ -9,6 +9,7 @@ export default function ViewExperience({experiences}) {
                     <div name='position' className="expIten position">Position: {exp.position}</div>
                     <div name='startDate' className="expIten startDate">From: {exp.startDate}</div>
                     <div name='endDate' className="expIten endDate">To: {exp.endDate}</div>
+                    <button onClick={() => onDeleteExp(exp.id)}>Delete</button>
             </li>
             ))}
         </ul>
