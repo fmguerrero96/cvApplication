2d8407d39a0385d3220ca004a26630798b873fa7
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-02 19:09:28 -0400

    Wrap ul element with a div

--- src/components/viewExperience.js
+++ src/components/viewExperience.js
@@ -2,17 +2,19 @@ import React from "react";
 
 export default function ViewExperience({experiences, onDeleteExp}) {
     return(
-        <ul className="viewExperience">
-            {experiences.map((exp) => (
-                <li key={exp.id} className="experienceBlock">
-                    <div name='companyName' className="expItem companyName">Company: {exp.companyName}</div>
-                    <div name='position' className="expIten position">Position: {exp.position}</div>
-                    <div name='startDate' className="expIten startDate">From: {exp.startDate}</div>
-                    <div name='endDate' className="expIten endDate">To: {exp.endDate}</div>
-                    <button onClick={() => onDeleteExp(exp.id)}>Delete</button>
-            </li>
-            ))}
-        </ul>
+        <div className="expTitle"> Work Experience
+            <ul className="viewExperience">
+                {experiences.map((exp) => (
+                    <li key={exp.id} className="experienceBlock">
+                        <div name='companyName' className="expItem companyName">Company: {exp.companyName}</div>
+                        <div name='position' className="expIten position">Position: {exp.position}</div>
+                        <div name='startDate' className="expIten startDate">From: {exp.startDate}</div>
+                        <div name='endDate' className="expIten endDate">To: {exp.endDate}</div>
+                        <button onClick={() => onDeleteExp(exp.id)}>Delete</button>
+                </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
