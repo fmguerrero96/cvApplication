5597eb024b2ea6ac5a690b9401b3b8afc3dd7e27
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-02 19:51:21 -0400

    Wrap ul inside a div

--- src/components/viewEducation.js
+++ src/components/viewEducation.js
@@ -2,16 +2,18 @@ import React from "react";
 
 export default function ViewEducation({educations, onDeleteEdu}) {
     return(
-        <ul className="viewEducation">
-            {educations.map((edu) => (
-                <li key={edu.id} className="educationBlock">
-                    <div name='schoolName' className="eduItem schoolName">School: {edu.schoolName}</div>
-                    <div name='degree' className="eduIten degree">Degree: {edu.degree}</div>
-                    <div name='startSchool' className="eduItem startSchool">From: {edu.startSchool}</div>
-                    <div name='endSchool' className="eduItem endSchool">To: {edu.endSchool}</div>
-                    <button onClick={() => onDeleteEdu(edu.id)}>Delete</button>
-            </li>
-            ))}
-        </ul>
+        <div className="eduTitle"> Education
+            <ul className="viewEducation">
+                {educations.map((edu) => (
+                    <li key={edu.id} className="educationBlock">
+                        <div name='schoolName' className="eduItem schoolName">School: {edu.schoolName}</div>
+                        <div name='degree' className="eduIten degree">Degree: {edu.degree}</div>
+                        <div name='startSchool' className="eduItem startSchool">From: {edu.startSchool}</div>
+                        <div name='endSchool' className="eduItem endSchool">To: {edu.endSchool}</div>
+                        <button onClick={() => onDeleteEdu(edu.id)}>Delete</button>
+                </li>
+                ))}
+            </ul>
+        </div>
     )
 }
