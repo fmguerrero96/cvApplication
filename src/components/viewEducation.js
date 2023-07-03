808f40b10a4f13591b23ac1705c9844e511825c3
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-02 20:02:57 -0400

    Remove conflicting class names

--- src/components/viewEducation.js
+++ src/components/viewEducation.js
@@ -6,8 +6,8 @@ export default function ViewEducation({educations, onDeleteEdu}) {
             <ul className="viewEducation">
                 {educations.map((edu) => (
                     <li key={edu.id} className="educationBlock">
-                        <div name='schoolName' className="eduItem schoolName">School: {edu.schoolName}</div>
-                        <div name='degree' className="eduIten degree">Degree: {edu.degree}</div>
+                        <div name='schoolName' className="eduItem">School: {edu.schoolName}</div>
+                        <div name='degree' className="eduIten">Degree: {edu.degree}</div>
                         <div name='startSchool' className="eduItem startSchool">From: {edu.startSchool}</div>
                         <div name='endSchool' className="eduItem endSchool">To: {edu.endSchool}</div>
                         <button onClick={() => onDeleteEdu(edu.id)}>Delete</button>
